0562a5ab5259b83d9e1f275ededaacb37222f61a
prateekagrawaliiit <prateekagrawaliiit@gmail.com>
2021-03-07 00:58:28 +0530

    Added links to profiles

--- app.py
+++ app.py
@@ -2,18 +2,17 @@
 # @Author: prateek
 # @Date:   2021-03-06 21:48:25
 # @Last Modified by:   prateek
-# @Last Modified time: 2021-03-06 22:16:11
+# @Last Modified time: 2021-03-07 00:58:16
 
 import streamlit as st 
 from PIL import Image
 import style
 
-st.title('DeepStyle')
+st.markdown("<h1 style='text-align: center;'>DeepStyle</h1>", unsafe_allow_html=True)
 st.markdown(
 	"""
 	**DeepStyle** is a Style Transfer web application implemented using transfer learning in PyTorch.
 	""")
-st.sidebar.title('DeepStyle')
 
 img = st.sidebar.selectbox('Select Image',('Amber','Cat'))
 if img=='Amber':
@@ -42,12 +41,21 @@ st.sidebar.header('Selected Style')
 sty_img = Image.open('./images/style-images/'+style_name+'.jpg')
 st.sidebar.image(sty_img,width=300)
 
-st.write("""Selected Source Image :""")
+st.sidebar.markdown("""#### DeepStyle is built and maintained by **Prateek Agrawal**. Please contact in case of queries or just to say Hi!!!.""")
+github = '[GitHub](http://github.com/prateekagrawaliiit)'
+linkedin = '[LinkedIn](https://www.linkedin.com/in/prateekagrawal1405/)'
+email = '<a href="mailto:prateekagrawaliiit@gmail.com">Email</a>'
+st.sidebar.markdown("""""")
+st.sidebar.markdown(github, unsafe_allow_html=True)
+st.sidebar.markdown(linkedin, unsafe_allow_html=True)
+st.sidebar.markdown(email, unsafe_allow_html=True)
+
+st.write("""- ### Selected Source Image :""")
 image = Image.open(input_img)
 st.image(image,width=500)
 
 
-stylize_btn = st.button("Stylize the selected Image")
+stylize_btn = st.button("- ### Stylize the selected Image")
 
 if stylize_btn:
 	model = style.load_model(saved_model)
